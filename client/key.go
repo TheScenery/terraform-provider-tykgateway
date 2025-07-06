a1a099237a186596d45587893005244ffc91a163
TheScenery <liuwenbin.lwb@gmail.com>
2025-07-06 22:47:00 +0800

    update for key model

--- client/key.go
+++ client/key.go
@@ -5,12 +5,142 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+
+	"github.com/TykTechnologies/graphql-go-tools/pkg/graphql"
 )
 
+type AccessSpec struct {
+	URL     string   `json:"url"`
+	Methods []string `json:"methods"`
+}
+type RateLimitSmoothing struct {
+	// Enabled indicates if rate limit smoothing is active.
+	Enabled bool `json:"enabled"`
+
+	// Threshold is the initial rate limit beyond which smoothing will be applied. It is a count of requests during the `per` interval and should be less than the maximum configured `rate`.
+	Threshold int64 `json:"threshold"`
+
+	// Trigger is a fraction (typically in the range 0.1-1.0) of the step at which point a smoothing event will be emitted as the request rate approaches the current allowance.
+	Trigger float64 `json:"trigger"`
+
+	// Step is the increment by which the current allowance will be increased or decreased each time a smoothing event is emitted.
+	Step int64 `json:"step"`
+
+	// Delay is a hold-off between smoothing events and controls how frequently the current allowance will step up or down (in seconds).
+	Delay int64 `json:"delay"`
+}
+type RateLimit struct {
+	// Rate is the allowed number of requests per interval.
+	Rate float64 `json:"rate"`
+	// Per is the interval at which rate limit is enforced.
+	Per float64 `json:"per"`
+
+	// Smoothing contains rate limit smoothing settings.
+	Smoothing *RateLimitSmoothing `json:"smoothing,omitempty"`
+}
+type APILimit struct {
+	RateLimit
+	ThrottleInterval   float64 `json:"throttle_interval"`
+	ThrottleRetryLimit int     `json:"throttle_retry_limit"`
+	MaxQueryDepth      int     `json:"max_query_depth"`
+	QuotaMax           int64   `json:"quota_max"`
+	QuotaRenews        int64   `json:"quota_renews"`
+	QuotaRemaining     int64   `json:"quota_remaining"`
+	QuotaRenewalRate   int64   `json:"quota_renewal_rate"`
+}
+type FieldAccessDefinition struct {
+	TypeName  string      `json:"type_name"`
+	FieldName string      `json:"field_name"`
+	Limits    FieldLimits `json:"limits"`
+}
+
+type FieldLimits struct {
+	MaxQueryDepth int `json:"max_query_depth"`
+}
+
+type BasicAuthData struct {
+	Password string `json:"password"`
+	Hash     string `json:"hash_type"`
+}
+
+type JWTData struct {
+	Secret string `json:"secret"`
+}
+
+type Monitor struct {
+	TriggerLimits []float64 `json:"trigger_limits"`
+}
+type Endpoint struct {
+	Path    string          `json:"path,omitempty"`
+	Methods EndpointMethods `json:"methods,omitempty"`
+}
+
+type EndpointMethod struct {
+	Name  string    `json:"name,omitempty"`
+	Limit RateLimit `json:"limit,omitempty"`
+}
+
+type EndpointMethods []EndpointMethod
+type Endpoints []Endpoint
+type AccessDefinition struct {
+	APIName              string                  `json:"api_name"`
+	APIID                string                  `json:"api_id"`
+	Versions             []string                `json:"versions"`
+	AllowedURLs          []AccessSpec            `json:"allowed_urls"` // mapped string MUST be a valid regex
+	RestrictedTypes      []graphql.Type          `json:"restricted_types"`
+	AllowedTypes         []graphql.Type          `json:"allowed_types"`
+	Limit                APILimit                `json:"limit"`
+	FieldAccessRights    []FieldAccessDefinition `json:"field_access_rights"`
+	DisableIntrospection bool                    `json:"disable_introspection"`
+	AllowanceScope       string                  `json:"allowance_scope"`
+	Endpoints            Endpoints               `json:"endpoints,omitempty"`
+}
+
 type Key struct {
+	LastCheck                     int64                       `json:"last_check"`
+	Allowance                     float64                     `json:"allowance"`
+	Rate                          float64                     `json:"rate"`
+	Per                           float64                     `json:"per"`
+	ThrottleInterval              float64                     `json:"throttle_interval"`
+	ThrottleRetryLimit            int                         `json:"throttle_retry_limit"`
+	MaxQueryDepth                 int                         `json:"max_query_depth"`
+	DateCreated                   string                      `json:"date_created"`
+	Expires                       int64                       `json:"expires"`
+	QuotaMax                      int64                       `json:"quota_max"`
+	QuotaRenews                   int64                       `json:"quota_renews"`
+	QuotaRemaining                int64                       `json:"quota_remaining"`
+	QuotaRenewalRate              int64                       `json:"quota_renewal_rate"`
+	AccessRights                  map[string]AccessDefinition `json:"access_rights"`
+	OrgID                         string                      `json:"org_id"`
+	OauthClientID                 string                      `json:"oauth_client_id"`
+	OauthKeys                     map[string]string           `json:"oauth_keys"`
+	Certificate                   string                      `json:"certificate"`
+	BasicAuthData                 BasicAuthData               `json:"basic_auth_data"`
+	JWTData                       JWTData                     `json:"jwt_data"`
+	HMACEnabled                   bool                        `json:"hmac_enabled"`
+	EnableHTTPSignatureValidation bool                        `json:"enable_http_signature_validation"`
+	HmacSecret                    string                      `json:"hmac_string"`
+	RSACertificateId              string                      `json:"rsa_certificate_id"`
+	IsInactive                    bool                        `json:"is_inactive"`
+	ApplyPolicyID                 string                      `json:"apply_policy_id"`
+	ApplyPolicies                 []string                    `json:"apply_policies"`
+	DataExpires                   int64                       `json:"data_expires"`
+	Monitor                       Monitor                     `json:"monitor"`
+	EnableDetailedRecording       bool                        `json:"enable_detailed_recording"`
+	MetaData                      map[string]interface{}      `json:"meta_data"`
+	Tags                          []string                    `json:"tags"`
+	Alias                         string                      `json:"alias"`
+	LastUpdated                   string                      `json:"last_updated"`
+	IdExtractorDeadline           int64                       `json:"id_extractor_deadline"`
+	SessionLifetime               int64                       `json:"session_lifetime"`
+	Smoothing                     *RateLimitSmoothing         `json:"smoothing"`
 }
 
 type ApiModifyKeySuccess struct {
+	Key     string `json:"key"`
+	Status  string `json:"status"`
+	Action  string `json:"action"`
+	KeyHash string `json:"key_hash,omitempty"`
 }
 
 func (c *Client) CreateKey(key Key) (ApiModifyKeySuccess, error) {
--- go.mod
+++ go.mod
@@ -4,27 +4,58 @@ go 1.24.4
 
 require github.com/hashicorp/terraform-plugin-framework v1.15.0
 
+require github.com/TykTechnologies/graphql-go-tools v1.6.2-0.20250606091303-a8e1ade2da8e
+
 require (
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/Masterminds/sprig v2.22.0+incompatible // indirect
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/coder/websocket v1.8.12 // indirect
+	github.com/eclipse/paho.mqtt.golang v1.2.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/go-hclog v1.5.0 // indirect
 	github.com/hashicorp/go-plugin v1.6.3 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/terraform-plugin-go v0.27.0 // indirect
 	github.com/hashicorp/terraform-plugin-log v0.9.0 // indirect
 	github.com/hashicorp/terraform-registry-address v0.2.5 // indirect
 	github.com/hashicorp/terraform-svchost v0.1.1 // indirect
 	github.com/hashicorp/yamux v0.1.1 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/huandu/xstrings v1.2.1 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/jensneuse/abstractlogger v0.0.4 // indirect
+	github.com/jensneuse/byte-template v0.0.0-20200214152254-4f3cf06e5c68 // indirect
+	github.com/jensneuse/pipeline v0.0.0-20200117120358-9fb4de085cd6 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mitchellh/copystructure v1.0.0 // indirect
 	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
+	github.com/mitchellh/reflectwalk v1.0.0 // indirect
 	github.com/oklog/run v1.0.0 // indirect
+	github.com/r3labs/sse/v2 v2.8.1 // indirect
+	github.com/santhosh-tekuri/jsonschema/v5 v5.3.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/tidwall/gjson v1.11.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/tidwall/sjson v1.0.4 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.18.1 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
 	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
 	google.golang.org/grpc v1.72.1 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
+	gopkg.in/cenkalti/backoff.v1 v1.1.0 // indirect
 )
